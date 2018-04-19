da7a1e18e672e4724b8be442a135cf52b47bd5b8
Benzwijaya <37841919+benzwijaya@users.noreply.github.com>
2018-04-20 02:55:51 +0700

    Add files via upload

---
+++ tugas6/tugas6/tugas6.py
@@ -0,0 +1,44 @@
+import mapnik
+m = mapnik.Map(800,400)
+m.background = mapnik.Color('steelblue')
+s = mapnik.Style()
+r = mapnik.Rule()
+polygon_symbolizer = mapnik.PolygonSymbolizer()
+polygon_symbolizer.fill = mapnik.Color('#f2eff9')
+r.symbols.append(polygon_symbolizer) 
+
+
+line_symbolizer = mapnik.LineSymbolizer(mapnik.Color('blue'), 3)
+
+
+r.symbols.append(line_symbolizer)
+s.rules.append(r)
+m.append_style('My Style1',s)
+ds = mapnik.Shapefile(file="../INDONESIA_KEC.shp")
+layer = mapnik.Layer('world')
+layer.datasource = ds
+layer.styles.append('My Style1')
+m.layers.append(layer)
+
+s = mapnik.Style()
+r = mapnik.Rule()
+polygon_symbolizer = mapnik.PolygonSymbolizer()
+polygon_symbolizer.fill = mapnik.Color('#f2eff9')
+r.symbols.append(polygon_symbolizer) 
+
+
+line_symbolizer = mapnik.LineSymbolizer(mapnik.Color('green'), 3)
+
+
+r.symbols.append(line_symbolizer)
+s.rules.append(r)
+m.append_style('My Style2',s)
+ds = mapnik.Shapefile(file="../garis.shp")
+layer = mapnik.Layer('world')
+layer.datasource = ds
+layer.styles.append('My Style2')
+m.layers.append(layer)
+
+m.zoom_all()
+mapnik.render_to_file(m, 'indo.pdf', 'pdf')
+print "rendered file to 'indo.pdf' "
